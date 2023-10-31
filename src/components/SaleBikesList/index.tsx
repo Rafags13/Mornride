81daa612e2f75d291dafb571922790139f91406a
Rafael Veiga <rafael_todescato@hotmail.com>
2023-10-31 10:04:38 -0400

    <fix>: temporary implementation to solve scroll bug by 1 item

--- src/components/SaleBikesList/index.tsx
+++ src/components/SaleBikesList/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import BikeCard from '../BikeCard'
-import { FlatList, Text, View } from 'react-native'
+import { FlatList, ScrollView, Text, View, ViewToken, ViewabilityConfig, VirtualizedList } from 'react-native'
 import { BikeCardsDto } from '../../util/model/BikeCardsDto'
 import { globalStyles } from '../../util/styles/global'
 
@@ -9,21 +9,36 @@ type Props = {
 }
 
 export default function SaleBikesList({ bikeCards }: Props) {
+    const flatlistReference = useRef<FlatList<BikeCardsDto>>(null);
+
+    function enableScrollWhenListCanScroll() {
+        return bikeCards.length > 1;
+        // Implementation for a possible bug that allows scrolling
+        // the list when has just 1 item
+    }
+
     return (
         <FlatList
             data={bikeCards}
+            ref={flatlistReference}
             style={{ marginVertical: 20 }}
+            contentOffset={{ x: 0, y: 0 }}
+            scrollEnabled={enableScrollWhenListCanScroll()}
+            onEndReached={(info: any) => {
+                if (info.distanceFromEnd >= 0 && bikeCards.length === 1) {
+                    flatlistReference.current?.scrollToOffset({ animated: true, offset: 0 });
+                }
+            }}
             contentContainerStyle={{
                 gap: 20
             }}
             horizontal
             showsHorizontalScrollIndicator={false}
-            renderItem={({ item, index }) => {
+            renderItem={({ item, index }: { item: BikeCardsDto, index: number }) => {
                 return (
                     <BikeCard key={item.id} bike={item} />
                 )
             }}
         />
-
     )
 }
