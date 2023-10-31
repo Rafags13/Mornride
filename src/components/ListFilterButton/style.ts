f6ac0af11b2abd78b64cbdd344580fc10d52c2d0
Rafael Veiga <rafael_todescato@hotmail.com>
2023-10-30 20:32:08 -0400

    <feat>: added skeleton to home

--- src/components/ListFilterButton/style.ts
+++ src/components/ListFilterButton/style.ts
@@ -3,13 +3,10 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
     contentContainer: {
         gap: 10,
+        marginVertical: 20,
         alignItems: 'center',
         justifyContent: 'center',
     },
-    flatlist: {
-        paddingVertical: 10,
-        marginVertical: 20
-    }
 })
 
 export default styles;
